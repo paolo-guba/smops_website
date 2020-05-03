5d889823f46f0403965c067b905394391ea4efa2
paolo-guba <44571808+paolo-guba@users.noreply.github.com>
2020-05-03 13:45:49 +0200

    added accordions in crew and research

---
+++ assets/js/accordionscript.js
@@ -0,0 +1,36 @@
+// var acc = document.getElementsByClassName("accordion");
+// var panel = document.getElementsByClassName('panel');
+
+// for (var i = 0; i < acc.length; i++) {
+//     acc[i].onclick = function() {
+//         var setClasses = !this.classList.contains('active');
+//         setClass(acc, 'active', 'remove');
+//         setClass(panel, 'show', 'remove');
+
+//         if (setClasses) {
+//             this.classList.toggle("active");
+//             this.nextElementSibling.classList.toggle("show");
+//         }
+//     }
+// }
+
+// function setClass(els, className, fnName) {
+//     for (var i = 0; i < els.length; i++) {
+//         els[i].classList[fnName](className);
+//     }
+// }
+
+var acc = document.getElementsByClassName("accordion");
+				var i;
+				
+				for (i = 0; i < acc.length; i++) {
+				  acc[i].addEventListener("click", function() {
+					this.classList.toggle("active");
+					var panel = this.nextElementSibling;
+					if (panel.style.maxHeight) {
+					  panel.style.maxHeight = null;
+					} else {
+					  panel.style.maxHeight = panel.scrollHeight + "px";
+					}
+				  });
+				}
